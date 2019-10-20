8640716b1c811ebd51cb0538c35004328170c6c1
iuin <826811175@qq.com>
2019-10-20 21:20:18 +0800

    更新配置文件

--- xmall-parent/src/main/resources/start-dubbo-admin.bat
+++ xmall-parent/src/main/resources/start-dubbo-admin.bat
@@ -1,2 +1,2 @@
-set address=10.62.229.205
+set address=127.0.0.1
 java -jar dubbo-admin.jar --admin.config-center=zookeeper://%address%:2181 --admin.registry.address=zookeeper://%address%:2181 --admin.metadata-report.address=zookeeper://%address%:2181
--- xmall-parent/src/main/resources/start-dubbo-admin.sh
+++ xmall-parent/src/main/resources/start-dubbo-admin.sh
@@ -1,2 +1,2 @@
-address=10.62.229.205
+address=127.0.0.1
 java -jar dubbo-admin.jar --admin.config-center=zookeeper://${address}:2181 --admin.registry.address=zookeeper://${address}:2181 --admin.metadata-report.address=zookeeper://${address}:2181
